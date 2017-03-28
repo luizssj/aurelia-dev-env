378d846f1353a507f0eee22af2904c569bf0c755
luiz <luiz@luizs-MacBook-Pro.local>
2017-03-28 18:45:58 -0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+export class App {
+  configureRouter(config, router) {
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['', 'welcome'], name: 'welcome',      moduleId: './welcome',      nav: true, title: 'Welcome' },
+      { route: 'users',         name: 'users',        moduleId: './users',        nav: true, title: 'Github Users' },
+      { route: 'child-router',  name: 'child-router', moduleId: './child-router', nav: true, title: 'Child Router' },
+      { route: 'md-select',  name: 'md-select', moduleId: 'material/select/md-select', nav: true, title: 'Select' },
+      { route: 'md-button',  name: 'md-button', moduleId: 'material/button/md-button', nav: true, title: 'Button' },
+      { route: 'md-slider',  name: 'md-slider', moduleId: 'material/slider/md-slider', nav: true, title: 'Slider' },
+      { route: 'md-collapsible',  name: 'md-collapsible', moduleId: 'material/collapsible/md-collapsible', nav: true, title: 'Collapsible' }
+    ]);
+
+    this.router = router;
+  }
+}
---
+++ src/material/slider/md-slider.js
@@ -0,0 +1,8 @@
+export class MdSliderDemo {
+  slides = [
+    { img: 'http://lorempixel.com/580/250/nature/1', align: 'center', heading: 'This is our big Tagline!', subheading: '1Here is our small slogan.' },
+    { img: 'http://lorempixel.com/580/250/nature/2', align: 'left', heading: 'Left Aligned Caption', subheading: '2Here is our small slogan.' },
+    { img: 'http://lorempixel.com/580/250/nature/3', align: 'right', heading: 'Right Aligned Caption', subheading: '3Here is our small slogan.' },
+    { img: 'http://lorempixel.com/580/250/nature/4', align: 'center', heading: 'This is another big Tagline!', subheading: '4Here is our small slogan.' }
+  ];
+}
